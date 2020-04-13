daef60c7c49bb766bde3d45eb7816f1549bc1c12
Danster3 <45056464+Danster3@users.noreply.github.com>
2020-04-13 20:57:58 +1200

    Added SlideShow

--- js/VideoSettings.js
+++ js/VideoSettings.js
@@ -1,4 +1,5 @@
 var myVideo = document.getElementById("video1"); 
+var myVideo = document.getElementById("video2"); 
 
 function playPause() { 
   if (myVideo.paused) 
@@ -18,3 +19,17 @@ function makeSmall() {
 function makeNormal() { 
     myVideo.width = 420; 
 } 
+
+let page = document.documentElement;
+
+page.style.setProperty('--size--','auto');
+var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+var element = document.getElementById('text');
+if (isMobile) {
+    element.innerHTML = "You are using Mobile";
+    page.style.setProperty('--size--','300px');
+} else {
+  element.innerHTML = "You are using Desktop";
+  page.style.setProperty('--size--','auto');
+}
+
